6fdd59623bc3159d6f47614c9ed8bf998c462695
Stefan Grunert <stefan@aptoma.com>
2013-04-05 09:12:04 +0200

    still playing

---
+++ config.h
@@ -0,0 +1,16 @@
+/*
+ * config.h
+ *
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+namespace config {
+	static const uint8_t pasSensorPin = 2;
+	static const uint8_t wheelSensorPin = 3;
+	static const uint8_t brakeSensorPin = 4;
+	static const uint8_t throttleSensorPin = A0;
+};
+
+#endif /* CONFIG_H_ */
